b768ada60847a990f60b29d2d6b43f557c3dd1db
Mark Roland <radiowhir@gmail.com>
2019-05-04 23:10:40 -0700

    Added additional coordinates to drawTable

--- drawTable.js
+++ drawTable.js
@@ -1,8 +1,11 @@
 function drawTable(plotter_exceeded = false) {
+
+  let coordinate_labels = true;
+
   // Draw table surface
   noStroke();
   fill(220, 220, 220);
-  ellipse(width/2, height/2, 648, 648);
+  ellipse(width/2, height/2, width, height);
 
   // Draw plottable area
   rectMode(CENTER);
@@ -12,7 +15,7 @@ function drawTable(plotter_exceeded = false) {
   }
   strokeWeight(1);
   fill(215, 215, 215);
-  rect(width/2, height/2, max_x, max_y);
+  rect(width/2, height/2, (max_x/648) * width, (max_y/648) * height);
 
   // Draw crosshairs
   //*
@@ -21,4 +24,28 @@ function drawTable(plotter_exceeded = false) {
   line(0, height/2, width, height/2);
   noStroke();
   //*/
+
+  // Draw Theta-Rho border
+  stroke(200);
+  noFill();
+  ellipse(width/2, height/2, min((max_x - min_x), (max_y - min_y)), min((max_x - min_x), (max_y - min_y)));
+
+  if (coordinate_labels) {
+    noStroke();
+    fill(128, 128, 128)
+    textAlign(LEFT);
+    text("[1, 0°]", width/2 + min((max_x - min_x), (max_y - min_y))/2 + 2, height/2 + 4);
+    textAlign(RIGHT);
+    text("[1, 180°]", width/2 - min((max_x - min_x), (max_y - min_y))/2 - 2, height/2 + 4);
+  }
+
+  // Display plotter area
+  if (coordinate_labels) {
+    noStroke();
+    fill(128, 128, 128)
+    textAlign(LEFT);
+    text("[0,0]", width/2 - (max_x - min_x)/2, height/2 + (max_y - min_y)/2 + 12);
+    textAlign(RIGHT);
+    text("[" + max_x + "," + max_y + "]", width/2 + (max_x - min_x)/2, height/2 - (max_y - min_y)/2 - 4);
+  }
 }

9733c98396a677d51a0ee7f6679b9b5ec8355b2b
Mark Roland <radiowhir@gmail.com>
2019-05-13 23:07:58 -0700

    Added path animation
    This is close, but not quite correct for the endpoint circles on the two different types of paths. This should be refactored to work with the same indices.

--- drawPath.js
+++ drawPath.js
@@ -2,41 +2,57 @@
 Draw Path
 
 */
-function drawPath(path, pathWidth = 1, connected = true) {
+function drawPath(path, pathWidth = 1, connected = true, animated = true) {
 
+  noFill();
   stroke(128, 164, 200);
   strokeCap(ROUND);
+  strokeWeight(pathWidth);
 
   // Start transformation matrix
   push();
   scale(1, -1);
   translate(width/2, -height/2);
 
+  let i_max = path.length;
+  if (animated) {
+    i_max = draw_iteration % path.length;
+  }
+
+  // Draw entire path
+  stroke(240, 240, 240);
+  beginShape();
+  for (var i = 0; i < path.length; i++) {
+    vertex(path[i][0], path[i][1]);
+  }
+  endShape();
+
   // Draw as a continuous, connected line (stroke color cannot change)
   if (connected) {
 
-    noFill();
-    stroke(128, 190, 200);
-    strokeWeight(pathWidth);
-
-    // Draw shape
+    // Draw animated path
+    stroke(46, 200, 240);
     beginShape();
-    for (var i = 0; i < path.length; i++) {
+    for (var i = 0; i < i_max; i++) {
       vertex(path[i][0], path[i][1]);
     }
     endShape();
+
+    // Draw endpoint
+    noStroke();
+    if (animated) {
+      fill(255,255,0,128);
+      ellipse(path[i_max][0], path[i_max][1], ball_size/2, ball_size/2);
+    }
 
   } else {
 
     // Draw as disconnected line segments. Stroke color can change with each segment
 
-    for (var i = 0; i < path.length-1; i++) {
+    for (var i = 0; i < i_max; i++) {
 
       // Gradiated Stroke
       stroke(128, 24 * cos((i/path.length) * TWO_PI) + 164, 200);
-
-      // Thicken to represent ball path
-      strokeWeight(ball_size/5);
 
       // Draw line segment
       line(path[i][0], path[i][1], path[i+1][0], path[i+1][1]);
@@ -61,6 +77,13 @@ function drawPath(path, pathWidth = 1, connected = true) {
       //*/
 
       vertex(path[i][0], path[i][1]);
+    }
+
+    // Draw endpoint
+    noStroke();
+    if (animated) {
+      fill(255,255,0,128);
+      ellipse(path[i_max][0], path[i_max][1], ball_size/2, ball_size/2);
     }
 
   }

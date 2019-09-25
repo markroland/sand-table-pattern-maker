262c66a08dcbc63cd740d2c41df4fccdd7807f33
Mark Roland <radiowhir@gmail.com>
2019-09-24 20:38:29 -0700

    Update of Shape Morph

--- assets/js/patterns/ShapeMorph.js
+++ assets/js/patterns/ShapeMorph.js
@@ -8,66 +8,86 @@ class ShapeMorph {
 
     // Define the shape to spin
 
-    this.sides = 32;
+    this.sides = 64;
 
     let radius = Math.min(max_x/2, max_y/2);
 
-    // Construct array for Starting Shape (Square)
-    // Middle right side to bottom right corner
-    let i_max = this.sides/8;
-    this.base_shape = [];
-    for (var i = 0; i < i_max; i++) {
-      this.base_shape.push([
-        radius,
-        -((i/i_max) * radius),
-      ]);
-    }
-    // bottom right corner to bottom left corner
-    i_max = this.sides/4;
-    for (var i = 0; i < i_max; i++) {
-      this.base_shape.push([
-        radius - ((i/i_max) * (2 * radius)),
-        -radius
-      ]);
-    }
-    // bottom left corner to top left corner
-    i_max = this.sides/4;
-    for (var i = 0; i < i_max; i++) {
-      this.base_shape.push([
-        -radius,
-        -radius + ((i/i_max) * (2 * radius)),
-      ]);
-    }
-    // top left corner to top right corner
-    i_max = this.sides/4;
-    for (var i = 0; i < i_max; i++) {
-      this.base_shape.push([
-        -radius + ((i/i_max) * (2 * radius)),
-        radius
-      ]);
-    }
-    // top right corner to middle right side
-    i_max = this.sides/8;
-    for (var i = 0; i < i_max; i++) {
-      this.base_shape.push([
-        radius,
-        radius - ((i/i_max) * radius),
-      ]);
-    }
-    this.base_shape.push([radius,0]);
-
-    // Ending Shape (Circle)
-    var shape_sides = 32;
-    this.end_shape = [];
-    for (var j = 0; j <= (shape_sides+0); j++) {
-      this.end_shape.push([
-        radius * Math.cos((j/shape_sides) * 2 * Math.PI),
-        -radius * Math.sin((j/shape_sides) * 2 * Math.PI)
-      ]);
-    }
+    // Define shapes
+    this.circle = this.circleShape(this.sides, radius);
+    this.heart = this.heartShape(this.sides, radius);
+    this.square = this.squareShape(this.sides, radius);
 
     // Define the parametric equations using text inputs
     this.config = {
+      "startShape": {
+        "name": "Inside Shape",
+        "value": "square",
+        "input": {
+          "type": "createSelect",
+          "options": {
+            "circle": "Circle",
+            "heart": "Heart",
+            "square": "Square"
+          }
+        }
+      },
+      "endShape": {
+        "name": "Outside Shape",
+        "value": "circle",
+        "input": {
+          "type": "createSelect",
+          "options": {
+            "circle": "Circle",
+            "heart": "Heart",
+            "square": "Square"
+          }
+        }
+      },
+      "revolutions": {
+        "name": "Revolutions",
+        "value": 1,
+        "input": {
+          "type": "createSlider",
+          "params" : [
+            1,
+            40,
+            10,
+            1
+          ],
+          "class": "slider",
+          "displayValue": true
+        }
+      },
+      "twist": {
+        "name": "Twist",
+        "value": 0.5,
+        "input": {
+          "type": "createSlider",
+          "params" : [
+            -1.0,
+            1.0,
+            0.5,
+            0.01
+          ],
+          "class": "slider",
+          "displayValue": true
+        }
+      },
+      "completion": {
+        "name": "Completion",
+        "value": 0.5,
+        "input": {
+          "type": "createSlider",
+          "params" : [
+            0.1,
+            1.0,
+            0.5,
+            0.1
+          ],
+          "class": "slider",
+          "displayValue": true
+        }
+      },
       "reverse": {
         "name": "Reverse",
         "value": 0,
@@ -106,8 +126,20 @@ class ShapeMorph {
         .addClass('pattern-control');
 
       // Create the control form input
-      // TODO: make this dynamic
-      if (val.input.type == "createSlider") {
+      if (val.input.type == "createSelect") {
+        control.input = createSelect()
+          .attribute('name', key)
+          .parent(control.div)
+          .addClass(val.input.class);
+
+          const entries = Object.entries(val.input.options)
+          for (const [key, object] of entries) {
+            control.input.option(object, key);
+          }
+
+          control.input.value(val.value);
+
+      } else if (val.input.type == "createSlider") {
         control.input = createSlider(val.input.params[0], val.input.params[1], val.input.params[2], val.input.params[3])
           .attribute('name', key)
           .parent(control.div)
@@ -134,19 +166,30 @@ class ShapeMorph {
   draw() {
 
     // Update object
-    // this.config.steps.value = document.querySelector('#pattern-controls > div:nth-child(1) > input').value;
 
-    // Display selected values
-    // document.querySelector('#pattern-controls > div.pattern-control:nth-child(1) > span').innerHTML = this.config.steps.value;
+    var selected_outside_shape = document.querySelector('#pattern-controls > div:nth-child(1) > select').value;
+    var selected_inside_shape = document.querySelector('#pattern-controls > div:nth-child(2) > select').value;
+
+    this.config.revolutions.value = document.querySelector('#pattern-controls > div:nth-child(3) > input').value;
+    document.querySelector('#pattern-controls > div.pattern-control:nth-child(3) > span').innerHTML = this.config.revolutions.value;
+
+    this.config.twist.value = document.querySelector('#pattern-controls > div:nth-child(4) > input').value;
+    document.querySelector('#pattern-controls > div.pattern-control:nth-child(4) > span').innerHTML = this.config.twist.value;
+
+    this.config.completion.value = document.querySelector('#pattern-controls > div:nth-child(5) > input').value;
+    document.querySelector('#pattern-controls > div.pattern-control:nth-child(5) > span').innerHTML = (100 * this.config.completion.value) + "%";
 
     let path = this.calc(
-      this.base_shape,
-      this.end_shape,
-      this.sides
+      this[selected_outside_shape],
+      this[selected_inside_shape],
+      this.sides,
+      this.config.revolutions.value,
+      this.config.completion.value,
+      this.config.twist.value
     );
 
     // Reverse the path
-    if (document.querySelector('#pattern-controls > div:nth-child(1) > input[type=checkbox]').checked) {
+    if (document.querySelector('#pattern-controls > div:nth-child(6) > input[type=checkbox]').checked) {
       path.reverse();
     }
 
@@ -161,13 +204,11 @@ class ShapeMorph {
    *
    * @return Array Path
    **/
-  calc(base_shape, end_shape, steps_per_revolution) {
+  calc(base_shape, end_shape, steps_per_revolution, revolutions, completion, twist) {
 
     // Set initial values
     var x;
     var y;
-    var t = 0.0;
-    let revolutions = 20;
     let current_revolution;
 
     // Initialize return value - the path array
@@ -178,44 +219,144 @@ class ShapeMorph {
     var step = 0;
 
     let growth_factor;
+    let lerp_amount;
 
+    // Theta values
     const max_t = revolutions * TWO_PI;
+    const min_t = (1.0 - completion) * max_t;
+    let t = min_t;
 
     // Loop through revolutions
     while (t < max_t) {
 
       // Rotational Angle (steps per rotation in the denominator)
-      t = (step/steps_per_revolution) * TWO_PI;
+      t = (step/steps_per_revolution) * TWO_PI + min_t;
 
+      // Calculate current rotation
       current_revolution = Math.floor(t/TWO_PI);
 
-      // Debugging
-      // if (current_revolution > 0) {
-      //   return path;
-      // }
+      // Calculate x,y coordinates
+      // TODO: http://www.gizma.com/easing/#quint2
+      lerp_amount = (current_revolution/revolutions);
+      x = (t/max_t) * lerp(base_shape[step % steps_per_revolution][0], end_shape[step % steps_per_revolution][0], lerp_amount);
+      y = (t/max_t) * lerp(base_shape[step % steps_per_revolution][1], end_shape[step % steps_per_revolution][1], lerp_amount);
 
       // Rotate [x,y] coordinates around [0,0] by angle theta, and then append to path
-      path.push(
-        [
-          (t/max_t) * lerp(base_shape[step % steps_per_revolution][0], end_shape[step % steps_per_revolution][0], current_revolution/revolutions),
-          (t/max_t) * lerp(base_shape[step % steps_per_revolution][1], end_shape[step % steps_per_revolution][1], current_revolution/revolutions),
-        ]
+      path[step] = this.rotationMatrix(x, y, twist * t/steps_per_revolution);
 
-        // [
-        //   (t/max_t) * base_shape[step % steps_per_revolution][0],
-        //   (t/max_t) * base_shape[step % steps_per_revolution][1],
-        // ]
-
-        // [
-        //   (t/max_t) * end_shape[step % steps_per_revolution][0],
-        //   (t/max_t) * end_shape[step % steps_per_revolution][1],
-        // ]
-      );
+      path.push([x,y]);
 
       // Increment iteration counter
       step++;
     }
 
+    // Remove the last element of the path.
+    /*
+    I'm not sure why, but without this the last element is anchored at a point we don't want.
+    There's probably a more elegant way to handle this in the loop above, but for now
+    I'm going with this.
+    */
+    path.pop();
+
     return path;
+  }
+
+  /**
+   * Rotate points x and y by angle theta about center point (0,0)
+   * https://en.wikipedia.org/wiki/Rotation_matrix
+   **/
+  rotationMatrix(x, y, theta) {
+      return [
+        x * cos(theta) - y * sin(theta),
+        x * sin(theta) + y * cos(theta)
+      ];
+  }
+
+  circleShape(array_size, radius) {
+
+    let shape = [];
+
+    for (var i = 0; i <= (array_size+0); i++) {
+      shape.push([
+        radius * Math.cos((i/array_size) * 2 * Math.PI),
+        -radius * Math.sin((i/array_size) * 2 * Math.PI)
+      ]);
+    }
+
+    return shape;
+  }
+
+  heartShape(array_size, radius) {
+
+    let shape = [];
+
+    let x, y, theta;
+
+    for (var i = 0; i <= (array_size+0); i++) {
+      theta = ((i/array_size) * (2 * Math.PI)) + 0.5*Math.PI;
+      x = 10 * (16 * Math.pow(Math.sin(theta), 3));
+      y = 10 * (13 * Math.cos(theta) - 5 * Math.cos(2 * theta) - 2 * Math.cos(3 * theta) - Math.cos(4 * theta));
+      shape.push([x,y]);
+    }
+
+    return shape;
+  }
+
+  // TODO: Re-write this to go counter-clockwise
+  squareShape(array_size, radius) {
+
+    // Construct array for Starting Shape (Square)
+    // Middle right side to bottom right corner
+
+    let i_max = array_size/8;
+
+    let shape = [];
+
+    for (var i = 0; i < i_max; i++) {
+      shape.push([
+        radius,
+        -((i/i_max) * radius),
+      ]);
+    }
+
+    // bottom right corner to bottom left corner
+    i_max = array_size/4;
+    for (var i = 0; i < i_max; i++) {
+      shape.push([
+        radius - ((i/i_max) * (2 * radius)),
+        -radius
+      ]);
+    }
+
+    // bottom left corner to top left corner
+    i_max = array_size/4;
+    for (var i = 0; i < i_max; i++) {
+      shape.push([
+        -radius,
+        -radius + ((i/i_max) * (2 * radius)),
+      ]);
+    }
+
+    // top left corner to top right corner
+    i_max = array_size/4;
+    for (var i = 0; i < i_max; i++) {
+      shape.push([
+        -radius + ((i/i_max) * (2 * radius)),
+        radius
+      ]);
+    }
+
+    // top right corner to middle right side
+    i_max = array_size/8;
+    for (var i = 0; i < i_max; i++) {
+      shape.push([
+        radius,
+        radius - ((i/i_max) * radius),
+      ]);
+    }
+
+    shape.push([radius,0]);
+
+    return shape;
   }
 }

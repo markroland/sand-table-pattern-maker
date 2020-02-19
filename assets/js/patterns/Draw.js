e7e2adc5717b55e5ec571d9c5bcfe257ae843502
Mark Roland <radiowhir@gmail.com>
2020-02-18 21:52:55 -0800

    Added support for free drawing with mouse input

--- assets/js/patterns/Draw.js
+++ assets/js/patterns/Draw.js
@@ -13,16 +13,35 @@ class Draw {
   }
 
   setup() {
-    path = [0,0]
+    path = [[0,0]]
   }
 
   draw() {
 
+    let x;
+    let y;
+    let plotter_x, plotter_y;
+
     if (mouseIsPressed) {
-        path.push([
-            mouseX-width/2,
-            -(mouseY-height/2)
-        ])
+
+        // Translate Canvas coordinates to table coordinates
+        x = mouseX - width/2;
+        y = -(mouseY - height/2);
+
+        // Translate "centered" coordinates to plotter position
+        plotter_x = x + ((max_x - min_x)/2);
+        plotter_y = y + ((max_y - min_y)/2);
+
+        // Add to path if within plotter bounds
+        if (plotter_x > min_x && plotter_x < max_x && plotter_y > min_y && plotter_y < max_y) {
+
+            // On first press replace [0,0] initial value with x,y
+            if (path.length == 1 && path[0][0] == 0 && path[0][1] == 0) {
+                path = [[x,y]];
+            } else {
+                path.push([x,y]);
+            }
+        }
     }
 
     return path;

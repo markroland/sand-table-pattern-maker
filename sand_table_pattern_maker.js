markroland/sand-table-pattern-maker

commit b78f777b8d8f5d66994cfba8d0e056fc3365d6c3

Refactored Wiggly Spiral

sand_table_pattern_maker.js
@@ -47,6 +47,7 @@ var Patterns = {
   "spiral": new Spiral(),
   "spokes": new Spokes(),
   "star": new Star(),
+  "wigglyspiral": new WigglySpiral(),
   "zigzag": new ZigZag()
 }
 
@@ -105,14 +106,9 @@ function draw() {
   // Draw the background
   background(68);
 
-  // Calculate the pattern
-  switch(pattern_select.value()) {
-    case "Spiral - Modulated":
-      path = drawWigglySpiral();
-      break;
-    default:
-      path = Patterns[pattern_select.value()].draw();
-  }
+  // Draw selected pattern
+  var selected_pattern = pattern_select.value();
+  path = Patterns[selected_pattern].draw();
 
   // Optimize path
   // Remove step sizes less than a threshold ("units")
@@ -125,7 +121,7 @@ function draw() {
   drawPath(path, 2, true, true);
 
   // Calculate path length
-  distance = 0;
+  let distance = 0;
   for (i = 1; i < path.length; i++) {
     distance += sqrt(pow(path[i][0] - path[i-1][0], 2) + pow(path[i][1] - path[i-1][1], 2));
   }
@@ -137,9 +133,9 @@ function draw() {
 
   // Update the URL (and browser history)
   // https://zellwk.com/blog/looping-through-js-objects/
-  if (Patterns[pattern_select.value()] !== undefined) {
-    let query_string = '?pattern=' + pattern_select.value();
-    const entries = Object.entries(Patterns[pattern_select.value()].config)
+  if (Patterns[selected_pattern] !== undefined) {
+    let query_string = '?pattern=' + selected_pattern;
+    const entries = Object.entries(Patterns[selected_pattern].config)
     for (const [param, content] of entries) {
       query_string = query_string.concat(`&${param}=${content.value}`)
     }
@@ -193,13 +189,9 @@ function patternSelectEvent() {
   // Clear controls
   select('#pattern-controls').html('');
 
-  switch(pattern_select.value()) {
-    case "Spiral - Modulated":
-      setupWigglySpiral();
-      break;
-    default:
-      Patterns[pattern_select.value()].setup();
-  }
+  // Call setup on selected pattern
+  var selected_pattern = pattern_select.value();
+  Patterns[selected_pattern].setup();
 
   // Change document title
   document.title = 'Sand Pattern | ' + pattern_select.value();

assets/js/patterns/WigglySpiral.js
@@ -1,0 +1,202 @@
+/**
+ * Wiggly Spiral
+ */
+class WigglySpiral {
+
+  constructor() {
+
+    this.key = "wigglyspiral";
+
+    this.name = "Wiggly Spiral";
+
+    this.config = {
+      "offset": {
+        "name": "Offset",
+        "value": 20,
+        "input": {
+          "type": "createSlider",
+          "params" : [
+            2,
+            40,
+            20,
+            1
+          ],
+          "class": "slider",
+          "displayValue": true
+        }
+      },
+      "amplitude": {
+        "name": "Amplitude",
+        "value": 5,
+        "input": {
+          "type": "createSlider",
+          "params" : [
+            1,
+            10,
+            5,
+            0.1
+          ],
+          "class": "slider",
+          "displayValue": true
+        }
+      },
+      "wiggles": {
+        "name": "Wiggles/Rev",
+        "value": 20,
+        "input": {
+          "type": "createSlider",
+          "params" : [
+            0,
+            40,
+            20,
+            0.1
+          ],
+          "class": "slider",
+          "displayValue": true
+        }
+      }
+    };
+
+    this.path = [];
+  }
+
+  /**
+   * Setup the controls for the pattern.
+   *
+   * @return Null
+   **/
+  setup() {
+
+    let controls = new Array();
+    const configs = Object.entries(this.config);
+    for (const [key, val] of configs) {
+
+      // Create a new object
+      var control = new Object();
+
+      // Create the div that contains the control
+      control.div = createDiv('<label>' + val.name + '</label>')
+        .parent('pattern-controls')
+        .addClass('pattern-control');
+
+      // Create the control form input
+      // TODO: make this dynamic
+      if (val.input.type == "createSlider") {
+        control.input = createSlider(val.input.params[0], val.input.params[1], val.input.params[2], val.input.params[3])
+          .attribute('name', key)
+          .parent(control.div)
+          .addClass(val.input.class);
+      } else if (val.input.type == "createInput") {
+        control.input = createInput(val.input.params[0], val.input.params[1], val.input.params[2])
+          .attribute("type", "checkbox")
+          .attribute('name', key)
+          .attribute('checkbox', null)
+          .parent(control.div);
+      }
+
+      // Create a span element to display the current input's value (useful for Sliders)
+      if (val.input.displayValue) {
+        let radius_value = createSpan('0')
+          .parent(control.div);
+      }
+
+      // Add to "controls" object
+      controls.push(control);
+    }
+  }
+
+  /**
+   * Draw path - Use class's "calc" method to convert inputs to a draw path
+   */
+  draw() {
+
+    // Read in selected value(s)
+    this.config.offset.value = document.querySelector('#pattern-controls > div:nth-child(1) > input').value;
+    this.config.amplitude.value = document.querySelector('#pattern-controls > div:nth-child(2) > input').value;
+    this.config.wiggles.value = document.querySelector('#pattern-controls > div:nth-child(3) > input').value;
+
+    // Display selected value(s)
+    document.querySelector('#pattern-controls > div.pattern-control:nth-child(1) > span').innerHTML = this.config.offset.value + " " + units;
+    document.querySelector('#pattern-controls > div.pattern-control:nth-child(2) > span').innerHTML = this.config.amplitude.value + " " + units;
+    document.querySelector('#pattern-controls > div.pattern-control:nth-child(3) > span').innerHTML = this.config.wiggles.value;
+
+    // Calculate path
+    let path = this.calc(
+        0,
+        0,
+        0.9 * (max_y/2),
+        0,
+        this.config.offset.value,
+        this.config.amplitude.value,
+        this.config.wiggles.value
+    );
+
+    // Update object
+    this.path = path;
+
+    return path;
+  }
+
+  /**
+   * Calculate coordinates for the shape
+   *
+   * @param integer Revolutions
+   *
+   * @return Array Path
+   **/
+  calc(start_x, start_y, start_r, start_theta, distance_between_turns, wiggle_amplitude, wiggle_frequency) {
+
+    // Set initial values
+    var x;
+    var y;
+    var r = start_r;
+    var theta = start_theta;
+
+    // Calculate the maximum radius
+    var max_r = min(max_x/2, max_y/2);
+
+    // Initialize shape path array
+    // This stores the x,y coordinates for each step
+    var path = new Array();
+
+    // Iteration counter.
+    var step = 0;
+
+    // Increase the denominator to get finer resolution (more instructions/longer time to plot)
+    var theta_per_step = 1/300;
+
+    // Continue as long as the design stays within bounds of the plotter
+    // This isn't quite right yet. I need to look into the coordinate translations
+    // while (r < max_r && x > width/2-max_x/2 && x < width/2+max_x/2 && y > height/2-max_y/2 && y < height/2-max_y/2) {
+    while (r > 0) {
+    // while (theta < 100 * TWO_PI) {
+
+      // Rotational Angle (steps per rotation in the denominator)
+      theta = step * theta_per_step * TWO_PI;
+
+      // Decrement the radius by a set amount per rotation
+      // Every full rotation the radius is reduced by the offset (distance_between_turns)
+      r = start_r - distance_between_turns * (step * theta_per_step);
+
+      // Optional: Decay Frequency and Amplitude
+      // wiggle_frequency = 0.9999 * wiggle_frequency;
+      // wiggle_amplitude = 0.99999 * wiggle_amplitude;
+
+      // Add a wiggle with a constant amplitude
+      // Subtract from radius so that drawing area will not be exceeded
+      r = r - wiggle_amplitude * sin(wiggle_frequency * theta);
+
+      // Convert polar position to rectangular coordinates
+      x = start_x + (r * cos(theta));
+      y = start_y + (r * sin(theta));
+
+      // Add coordinates to shape array
+      path[step] = [x,y];
+
+      // Increment iteration counter
+      step++;
+    }
+
+    return path;
+  }
+}

assets/js/patterns/calcWigglySpiral.js
@@ -1,116 +1,0 @@
-/**
- * Use this in setup() to set up the Pattern's controls
- */
-function setupWigglySpiral()
-{
-
-  // Offset control
-  offset = createDiv('<label>Offset</label>')
-    .parent('pattern-controls')
-    .addClass('pattern-control');
-  spiral_offset = createSlider(2, 40, 20);
-  spiral_offset.parent(offset);
-  offset_value = createSpan('0');
-  offset_value.parent(offset);
-
-  // Wiggle Amplitude
-  wiggle_amplitude_div = createDiv('<label>Amplitude</label>')
-    .parent('pattern-controls')
-    .addClass('pattern-control');
-  wiggle_amplitude = createSlider(1, 10, 5, 0.1);
-  wiggle_amplitude.parent(wiggle_amplitude_div);
-  wiggle_amplitude_value = createSpan('0');
-  wiggle_amplitude_value.parent(wiggle_amplitude_div);
-
-  // Wiggle Wavelength
-  wiggle_wavelength_div = createDiv('<label>Wiggles/Rev</label>')
-    .parent('pattern-controls')
-    .addClass('pattern-control');
-  wiggle_wavelength = createSlider(0, 40, 20, 0.1);
-  wiggle_wavelength.parent(wiggle_wavelength_div);
-  wiggle_wavelength_value = createSpan('0');
-  wiggle_wavelength_value.parent(wiggle_wavelength_div);
-}
-
-/**
- * Use this in draw() to compose the Pattern
- */
-function drawWigglySpiral()
-{
-
-  // Draw control selection values
-  offset_value.html(spiral_offset.value() + " " + units);
-  wiggle_amplitude_value.html(wiggle_amplitude.value() + " " + units);
-  wiggle_wavelength_value.html(wiggle_wavelength.value());
-
-  // Calculate a Spiral path
-  return calcWigglySpiral(
-    0,
-    0,
-    0.9 * (max_y/2),
-    0,
-    spiral_offset.value(),
-    wiggle_amplitude.value(),
-    wiggle_wavelength.value()
-  );
-
-}
-
-/**
- * Calculate coordinates for a Wiggly Spiral
- **/
-function calcWigglySpiral(start_x, start_y, start_r, start_theta, distance_between_turns, wiggle_amplitude, wiggle_frequency) {
-
-  // Set initial values
-  var x;
-  var y;
-  var r = start_r;
-  var theta = start_theta;
-
-  // Calculate the maximum radius
-  var max_r = min(max_x/2, max_y/2);
-
-  // Initialize shape path array
-  // This stores the x,y coordinates for each step
-  var path = new Array();
-
-  // Iteration counter.
-  var step = 0;
-
-  // Increase the denominator to get finer resolution (more instructions/longer time to plot)
-  var theta_per_step = 1/300;
-
-  // Continue as long as the design stays within bounds of the plotter
-  // This isn't quite right yet. I need to look into the coordinate translations
-  // while (r < max_r && x > width/2-max_x/2 && x < width/2+max_x/2 && y > height/2-max_y/2 && y < height/2-max_y/2) {
-  while (r > 0) {
-  // while (theta < 100 * TWO_PI) {
-
-    // Rotational Angle (steps per rotation in the denominator)
-    theta = step * theta_per_step * TWO_PI;
-
-    // Decrement the radius by a set amount per rotation
-    // Every full rotation the radius is reduced by the offset (distance_between_turns)
-    r = start_r - distance_between_turns * (step * theta_per_step);
-
-    // Optional: Decay Frequency and Amplitude
-    // wiggle_frequency = 0.9999 * wiggle_frequency;
-    // wiggle_amplitude = 0.99999 * wiggle_amplitude;
-
-    // Add a wiggle with a constant amplitude
-    // Subtract from radius so that drawing area will not be exceeded
-    r = r - wiggle_amplitude * sin(wiggle_frequency * theta);
-
-    // Convert polar position to rectangular coordinates
-    x = start_x + (r * cos(theta));
-    y = start_y + (r * sin(theta));
-
-    // Add coordinates to shape array
-    path[step] = [x,y];
-
-    // Increment iteration counter
-    step++;
-  }
-
-  return path;
-}

index.html
@@ -29,8 +29,8 @@
   <script language="javascript" type="text/javascript" src="assets/js/patterns/Spiral.js"></script>
   <script language="javascript" type="text/javascript" src="assets/js/patterns/Spokes.js"></script>
   <script language="javascript" type="text/javascript" src="assets/js/patterns/Star.js"></script>
+  <script language="javascript" type="text/javascript" src="assets/js/patterns/WigglySpiral.js"></script>
   <script language="javascript" type="text/javascript" src="assets/js/patterns/ZigZag.js"></script>
-  <script language="javascript" type="text/javascript" src="assets/js/patterns/calcWigglySpiral.js"></script>
 
   <script language="javascript" type="text/javascript" src="drawPath.js"></script>
   <script language="javascript" type="text/javascript" src="drawTable.js"></script>

commit 9e38e35206287db79b065ef1aaa9177c96810296

Fixed variable scope bug
Introduced earlier today in b78f777b

sand_table_pattern_maker.js
@@ -121,7 +121,7 @@ function draw() {
   drawPath(path, 2, true, true);
 
   // Calculate path length
-  let distance = 0;
+  distance = 0;
   for (i = 1; i < path.length; i++) {
     distance += sqrt(pow(path[i][0] - path[i-1][0], 2) + pow(path[i][1] - path[i-1][1], 2));
   }
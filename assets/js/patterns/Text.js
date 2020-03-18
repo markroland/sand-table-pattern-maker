ef277b4eb1ed69573a1ab335068d7d62a17afe4a
Mark Roland <radiowhir@gmail.com>
2020-03-17 23:10:34 -0700

    Defined alphabet characters

--- assets/js/patterns/Text.js
+++ assets/js/patterns/Text.js
@@ -138,32 +138,337 @@ class Text {
 
   draw_character(x, y, character) {
 
+    // Initialize path
     let path = new Array();
 
+    // Shorten variables
+    let height = this.line_height;
+    let width = this.char_width;
+
+    // Convert to upper case
+    character = character.toUpperCase();
+
+    // Character definitions
     switch(character) {
       case " ":
         path = [
-          [this.char_width, 0]
+          [width, 0]
         ];
         break;
       case "A":
         path = [
-          [0,0],
-          [this.char_width/2, this.line_height],
-          [0.75 * this.char_width, this.line_height/2],
-          [0.25 * this.char_width, this.line_height/2],
-          [0.75 * this.char_width, this.line_height/2],
-          [this.char_width, 0]
+          [0, 0],
+          [width/2, height],
+          [0.75 * width, height/2],
+          [0.25 * width, height/2],
+          [0.75 * width, height/2],
+          [width, 0]
+        ];
+        break;
+      case "B":
+        path = [
+          [0, 0],
+          [0, height],
+          [width, height],
+          [width, height/2],
+          [0, height/2],
+          [width, height/2],
+          [width, 0],
+          [0, 0]
+        ];
+        break;
+      case "C":
+        path = [
+          [0, 0],
+          [0, height],
+          [width, height],
+          [0, height],
+          [0, 0],
+          [width, 0],
+          [0, 0]
+        ];
+        break;
+      case "D":
+        path = [
+          [0, 0],
+          [0, height],
+          [width/4, height],
+          [(3/4) * width, height],
+          [width, (3/4) * height],
+          [width, (1/4) * height],
+          [(3/4) * width, 0],
+          [0, 0]
+        ];
+        break;
+      case "E":
+        path = [
+          [0, 0],
+          [0, height],
+          [width, height],
+          [0, height],
+          [0, height/2],
+          [width, height/2],
+          [0, height/2],
+          [0, 0],
+          [width, 0]
+        ];
+        break;
+      case "F":
+        path = [
+          [0, 0],
+          [0, height],
+          [width, height],
+          [0, height],
+          [0, height/2],
+          [width, height/2],
+          [0, height/2],
+          [0, 0]
+        ];
+        break;
+      case "G":
+        path = [
+          [0, 0],
+          [0, height],
+          [width, height],
+          [0, height],
+          [0, 0],
+          [width, 0],
+          [width, height/2],
+          [width/2, height/2],
+          [width, height/2],
+          [width, 0],
+          [0, 0]
+        ];
+        break;
+      case "H":
+        path = [
+          [0, 0],
+          [0, height],
+          [0, height/2],
+          [width, height/2],
+          [width, height],
+          [width, 0]
+        ];
+        break;
+      case "I":
+        path = [
+          [width/2, 0],
+          [width/2, height],
+          [width/2, 0]
+        ];
+        break;
+      case "J":
+        path = [
+          [0, 0],
+          [0, height/4],
+          [0, 0],
+          [width, 0],
+          [width, height],
+          [(3/4) * width, height],
+          [width, height],
+          [width, 0]
+        ];
+        break;
+      case "K":
+        path = [
+          [0, 0],
+          [0, height],
+          [0, height/2],
+          [width, height],
+          [0, height/2],
+          [width, 0],
+        ];
+        break;
+      case "L":
+        path = [
+          [0, 0],
+          [0, height],
+          [0, 0],
+          [width, 0]
+        ];
+        break;
+      case "M":
+        path = [
+          [0, 0],
+          [0, height],
+          [width/2, height/2],
+          [width, height],
+          [width, 0]
+        ];
+        break;
+      case "N":
+        path = [
+          [0, 0],
+          [0, height],
+          [width, 0],
+          [width, height],
+          [width, 0]
+        ];
+        break;
+      case "O":
+        path = [
+          [width/4, 0],
+          [0, height/4],
+          [0, (3/4) * height],
+          [width/4, height],
+          [(3/4) * width, height],
+          [width, (3/4) * height],
+          [width, (1/4) * height],
+          [(3/4) * width, 0],
+          [0, 0]
+        ];
+        break;
+      case "P":
+        path = [
+          [0, 0],
+          [0, height],
+          [(3/4) * width, height],
+          [width, (7/8) * height],
+          [width, (5/8) * height],
+          [(3/4) * width, (1/2) * height],
+          [0, (1/2) * height],
+          [0, 0]
+        ];
+        break;
+      case "Q":
+        path = [
+          [width/4, 0],
+          [0, height/4],
+          [0, (3/4) * height],
+          [width/4, height],
+          [(3/4) * width, height],
+          [width, (3/4) * height],
+          [width, (1/4) * height],
+          [(3/4) * width, 0],
+          [0, 0],
+          [(3/4) * width, 0],
+          [width, -height/4],
+          [(3/4) * width, 0]
+        ];
+        break;
+      case "R":
+        path = [
+          [0, 0],
+          [0, height],
+          [(3/4) * width, height],
+          [width, (7/8) * height],
+          [width, (5/8) * height],
+          [(3/4) * width, (1/2) * height],
+          [0, (1/2) * height],
+          [(3/4) * width, (1/2) * height],
+          [width, 0]
+        ];
+        break;
+      case "S":
+        path = [
+          [width/4, 0],
+          [0, (1/8) * height],
+          [width/4, 0],
+          [(3/4) * width, 0],
+          [width, (1/8) * height],
+          [width, (3/8) * height],
+          [(3/4) * width, (1/2) * height],
+          [width/4, (1/2) * height],
+          [0, (5/8) * height],
+          [0, (7/8) * height],
+          [width/4, height],
+          [(3/4) * width, height],
+          [width, (7/8) * height],
+          [(3/4) * width, height],
+          [width/4, height],
+          [0, (7/8) * height],
+          [0, (5/8) * height],
+          [width/4, (1/2) * height],
+          [(3/4) * width, (1/2) * height],
+          [width, (3/8) * height],
+          [width, (1/8) * height],
+          [(3/4) * width, 0],
+        ];
+        break;
+      case "T":
+        path = [
+          [width/2, 0],
+          [width/2, height],
+          [0, height],
+          [width, height],
+          [width/2, height],
+          [width/2, 0]
+        ];
+        break;
+      case "U":
+        path = [
+          [width/4, 0],
+          [0, height/4],
+          [0, height],
+          [0, height/4],
+          [width/4, 0],
+          [(3/4) * width, 0],
+          [width, (1/4) * height],
+          [width, height],
+          [width, (1/4) * height],
+          [(3/4) * width, 0]
+        ];
+        break;
+      case "V":
+        path = [
+          [width/2, 0],
+          [0, height],
+          [width/2, 0],
+          [width, height],
+          [width/2, 0]
+        ];
+        break;
+      case "W":
+        path = [
+          [0, 0],
+          [0, height],
+          [0, 0],
+          [width/2, height/2],
+          [width, 0],
+          [width, height],
+          [width, 0]
+        ];
+        break;
+      case "X":
+        path = [
+          [0, 0],
+          [width/2, height/2],
+          [0, height],
+          [width/2, height/2],
+          [width, height],
+          [width/2, height/2],
+          [width, 0]
+        ];
+        break;
+      case "Y":
+        path = [
+          [width/2, 0],
+          [width/2, height/2],
+          [0, height],
+          [width/2, height/2],
+          [width, height],
+          [width/2, height/2],
+          [width/2, 0]
+        ];
+        break;
+      case "Z":
+        path = [
+          [0, 0],
+          [width, height],
+          [0, height],
+          [width, height],
+          [0, 0],
+          [width, 0]
         ];
         break;
       default:
         path = [
           [0, 0],
-          [0, this.line_height],
-          [this.char_width, this.line_height],
-          [this.char_width, 0],
+          [0, height],
+          [width, height],
+          [width, 0],
           [0, 0],
-          [this.char_width, 0]
+          [width, 0]
         ];
     }
 

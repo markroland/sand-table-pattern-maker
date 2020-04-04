874c89742eb650cd2b6ea75941ba04202edb50fb
Mark Roland <radiowhir@gmail.com>
2020-04-03 21:57:34 -0700

    Added Starburst behind Cross
    - Removed pattern reverse

--- assets/js/patterns/Cross.js
+++ assets/js/patterns/Cross.js
@@ -52,16 +52,16 @@ class Cross {
           "displayValue": true
         }
       },
-      "reverse": {
-        "name": "Reverse",
+      "starburst": {
+        "name": "Starburst",
         "value": 0,
         "input": {
           "type": "createCheckbox",
           "attributes" : [{
             "type" : "checkbox",
-            "checked" : null
+            "checked" : true
           }],
-          "params": [0, 1, 0],
+          "params": [0, 1, 1],
           "displayValue": false
         }
       }
@@ -77,6 +77,11 @@ class Cross {
     this.config.height.value = document.querySelector('#pattern-controls > div:nth-child(2) > input').value;
     this.config.intersect.value = document.querySelector('#pattern-controls > div:nth-child(3) > input').value;
 
+    this.config.starburst.value = false;
+    if (document.querySelector('#pattern-controls > div:nth-child(4) > input[type=checkbox]').checked) {
+      this.config.starburst.value = true;
+    }
+
     // Display selected values
     document.querySelector('#pattern-controls > div.pattern-control:nth-child(1) > span').innerHTML = this.config.width.value + " " + units;
     document.querySelector('#pattern-controls > div.pattern-control:nth-child(2) > span').innerHTML = this.config.height.value + " " + units;
@@ -86,13 +91,9 @@ class Cross {
     let path = this.calc(
       parseInt(this.config.width.value),
       parseInt(this.config.height.value),
-      parseFloat(this.config.intersect.value/100)
+      parseFloat(this.config.intersect.value/100),
+      this.config.starburst.value
     );
-
-    // If pattern is reversed, then remove first (which will be last) point from path
-    if (document.querySelector('#pattern-controls input[name=reverse]').checked) {
-      path.shift();
-    }
 
     // Update object
     this.path = path;
@@ -109,11 +110,11 @@ class Cross {
    *
    * @return Array Path
    **/
-  calc(cross_width, cross_height, cross_intersect) {
+  calc(cross_width, cross_height, cross_intersect, starburst) {
 
     // Initialize return value - the path array
     // This stores the x,y coordinates for each step
-    var path = new Array([0,0]);
+    var path = new Array();
 
     let y_center = (cross_intersect * cross_height) - (cross_height/2);
 
@@ -121,9 +122,49 @@ class Cross {
     let rotations = 3;
     var offset;
 
+    if (starburst) {
+      path.push([0, y_center]);
+      let r;
+      let rx;
+      let ry;
+      let r_scale = 1.0;
+      let max_r = Math.min(max_x - min_x, max_y - min_y);
+      for (var i = 0; i < 16; i++){
+
+        // Calculate ellipse radius
+        rx = (cross_width / 2) + ((rotations + 3) * increment);
+        ry = (cross_height / 2) + ((rotations + 3) * increment) - y_center;
+
+        // Extend rays of the bottom half
+        if (i/16 > 0.5) {
+          ry = (cross_height / 2) + ((rotations + 3) * increment) + y_center;
+        }
+
+        // 45-degree rays
+        if ((i + 2) % 4 == 0) {
+          rx = 0.9 * rx;
+          ry = 0.9 * ry;
+        }
+
+        // Odd Rays
+        if ((i + 1) % 2 == 0) {
+          rx = 0.8 * rx;
+          ry = 0.8 * ry;
+        }
+
+        path.push([
+          rx * Math.cos(i/16 * (2 * Math.PI)),
+          ry * Math.sin(i/16 * (2 * Math.PI)) + y_center
+        ]);
+        path.push([0, y_center]);
+      }
+    }
+
+    // Cross path
+    var cross_path = new Array();
     for (var i = 0; i < rotations; i++) {
       offset = i * increment;
-      path = path.concat([
+      cross_path = cross_path.concat([
         [0 - offset, y_center - offset],
         [-cross_width/2 - offset, y_center - offset],
         [-cross_width/2 - offset, y_center + offset],
@@ -138,8 +179,16 @@ class Cross {
         [0 - offset, -cross_height/2 - offset],
         [0 - offset, y_center - offset]
       ]);
-
     }
+
+    // Reverse path
+    // This may be able to be accomplished more elegantly,
+    // but Array reverse was not working well for multi-dimensional arrays
+    var reverse_path = new Array();
+    for (var i=0; i < cross_path.length; i++) {
+      reverse_path.push(cross_path[(cross_path.length-1) - i]);
+    }
+    path = path.concat(reverse_path);
 
     // Return to center
     // path.push([0, y_center]);

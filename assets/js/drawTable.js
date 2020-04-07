f71a31a2ffed87f50a93907dbfe68138c62664af
Mark Roland <radiowhir@gmail.com>
2020-04-06 21:07:44 -0700

    Added support for Polar plotter overlay

--- assets/js/drawTable.js
+++ assets/js/drawTable.js
@@ -2,10 +2,14 @@ function drawTable(plotter_exceeded = false) {
 
   let coordinate_labels = true;
 
+  let table_radius = Math.min(max_x - min_x, max_y - min_y) / 2;
+
   // Draw table surface
   noStroke();
+  rectMode(CENTER);
   fill(220, 220, 220);
-  ellipse(width/2, height/2, width, height);
+  // ellipse(width/2, height/2, width, height);
+  rect(width/2, height/2, width, height);
 
   // Draw plottable area
   rectMode(CENTER);
@@ -15,37 +19,47 @@ function drawTable(plotter_exceeded = false) {
   }
   strokeWeight(1);
   fill(215, 215, 215);
-  rect(width/2, height/2, (max_x/648) * width, (max_y/648) * height);
+  if (env.table.format == "cartesian") {
+    rect(width/2, height/2, (max_x/648) * width, (max_y/648) * height);
+  } else if (env.table.format == "polar") {
+    ellipse(width/2, height/2, 2 * table_radius, 2 * table_radius);
+  }
 
   // Draw crosshairs
   //*
-  stroke(200);
+  stroke(210);
   line(width/2, 0, width/2, height);
   line(0, height/2, width, height/2);
   noStroke();
   //*/
 
   // Draw Theta-Rho border
-  stroke(200);
-  noFill();
-  ellipse(width/2, height/2, min((max_x - min_x), (max_y - min_y)), min((max_x - min_x), (max_y - min_y)));
+  if (env.table.format == "polar") {
+    stroke(200);
+    noFill();
+    ellipse(width/2, height/2, min((max_x - min_x), (max_y - min_y)), min((max_x - min_x), (max_y - min_y)));
 
-  if (coordinate_labels) {
-    noStroke();
-    fill(128, 128, 128)
-    textAlign(LEFT);
-    text("[1, 0°]", width/2 + min((max_x - min_x), (max_y - min_y))/2 + 2, height/2 + 4);
-    textAlign(RIGHT);
-    text("[1, 180°]", width/2 - min((max_x - min_x), (max_y - min_y))/2 - 2, height/2 + 4);
+    if (coordinate_labels) {
+      noStroke();
+      fill(128, 128, 128)
+      textAlign(LEFT);
+      text("[1, 0]", width/2 + min((max_x - min_x), (max_y - min_y))/2 + 2, height/2 + 4);
+      // textAlign(RIGHT);
+      // text("[1,π]", width/2 - min((max_x - min_x), (max_y - min_y))/2 - 2, height/2 + 4);
+      textAlign(CENTER);
+      text("[1, π/2]", width/2, table_radius - 60);
+    }
   }
 
   // Display plotter area
-  if (coordinate_labels) {
-    noStroke();
-    fill(128, 128, 128)
-    textAlign(LEFT);
-    text("[0,0]", width/2 - (max_x - min_x)/2, height/2 + (max_y - min_y)/2 + 12);
-    textAlign(RIGHT);
-    text("[" + max_x + "," + max_y + "]", width/2 + (max_x - min_x)/2, height/2 - (max_y - min_y)/2 - 4);
+  if (env.table.format == "cartesian") {
+    if (coordinate_labels) {
+      noStroke();
+      fill(128, 128, 128)
+      textAlign(LEFT);
+      text("[0,0]", width/2 - (max_x - min_x)/2, height/2 + (max_y - min_y)/2 + 12);
+      textAlign(RIGHT);
+      text("[" + max_x + "," + max_y + "]", width/2 + (max_x - min_x)/2, height/2 - (max_y - min_y)/2 - 4);
+    }
   }
 }

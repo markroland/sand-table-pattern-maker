6c0b814040f7099614e7887b22c3f4ecc8bc5ede
Mark Roland <radiowhir@gmail.com>
2020-04-18 15:43:39 -0700

    Fixed drawing implementation of Theta-Rho tracks

--- assets/js/patterns/ThetaRho.js
+++ assets/js/patterns/ThetaRho.js
@@ -6,6 +6,24 @@ class ThetaRho {
 
     this.name = "Theta Rho Coordinates";
 
+    let spiral_test = "# Start at Center\n0   0\n\n"
+      + "# Spiral out 4 revolutions\n"
+      + "25.13272 1\n\n"
+      + "# Do one full circle at maximum radius\n"
+      + "31.4159 1\n\n"
+      + "# do 40 revs in\n"
+      + "251.32.72  0\n"
+
+    let square_test = "0 0.5\n"
+      + "0.7854 0.7071\n"
+      + "1.5708 0.5\n"
+      + "2.3562 0.7071\n"
+      + "3.1416 0.5\n"
+      + "3.9270 0.7071\n"
+      + "4.7124 0.5\n"
+      + "5.4978 0.7071\n"
+      + "6.2832 0.5\n";
+
     this.config = {
       "thr": {
         "name": "Theta Rho",
@@ -16,13 +34,7 @@ class ThetaRho {
             "rows": 11,
             "cols": 30,
           },
-          "value" : "# Start at Center\n0   0\n\n"
-            + "# Spiral out 4 revolutions\n"
-            + "25.13272 1\n\n"
-            + "# Do one full circle at maximum radius\n"
-            + "31.4159 1\n\n"
-            + "# do 40 revs in\n"
-            + "251.32.72  0\n",
+          "value" : spiral_test,
           "params" : []
         }
       }
@@ -72,63 +84,44 @@ class ThetaRho {
 
     // Loop through lines and extract theta-rho instructions
     lines.forEach(function(element) {
-      let coordinates = element.match(/^([0-9\.]+)\s+([0-9\.]+)\s*$/)
+      let coordinates = element.match(/^([\+\-0-9\.]+)\s+([\+\-0-9\.]+)\s*$/)
       if (coordinates) {
         thr_commands.push([
-            parseFloat(coordinates[1]),
-            parseFloat(coordinates[2])
+          parseFloat(coordinates[1]),
+          parseFloat(coordinates[2])
         ]);
       }
     });
 
     // Convert starting point from Theta-Rho to XY
     path.push([
-        thr_commands[0][1] * Math.cos(thr_commands[0][0]),
-        thr_commands[0][1] * Math.sin(thr_commands[0][0])
+      max_r * thr_commands[0][1] * Math.cos(thr_commands[0][0]),
+      max_r * thr_commands[0][1] * Math.sin(thr_commands[0][0])
     ]);
 
-    // Loop through subsequent commands
-    for (var i = 1; i < thr_commands.length; i++) {
+    // Loop through Theta-Rho commands
+    let num_thr_commands = thr_commands.length
+    let substeps_per_revolution = 60;
+    let num_substeps;
+    for (var i = 1; i < num_thr_commands; i++) {
 
-        // Calculate the different between the current Rho (radius) value and the previous command's
-        var radius_difference = thr_commands[i][1] - thr_commands[i-1][1];
-
-        // Calculate the difference between the current Theta (angle) value and the previous command's
-        var theta_difference = thr_commands[i][0] - thr_commands[i-1][0];
-
-        // Calculate the number of steps to make based on how many revolutions of the circle are traveled
-        // Require at least one step
-        var steps_per_revolution = 60;
-        var steps = max(
-            Math.round((Math.abs(theta_difference) / (2 * Math.PI)) * steps_per_revolution),
-            1
-        );
-
-        // Loop through the steps between the two Theta-Rho commands and interpolate the XY coordinates
-        for (var j = 0; j <= steps; j++) {
-
-            // Calculate the small change (delta) of the Radius between the two steps
-            var radius_delta;
-            if (radius_difference > 0) {
-                radius_delta = max_r * (j / steps) * (radius_difference);
-            } else if (radius_difference < 0) {
-                radius_delta = max_r + (max_r * (j / steps) * (radius_difference));
-            } else {
-                radius_delta = max_r;
-            }
-
-            // Calculate the small change (delta) of the angle Theta between the two steps
-            var theta_delta = (j / steps) * (thr_commands[i][0] - thr_commands[i-1][0]);
-
-            // Convert to cartesian coordinates
-            // The negative sign on the Y Coordinate is there to reflect how I believe
-            // Theta-Rho is interpreted, but I don't have a machine to verify this.
-            path.push([
-                radius_delta * Math.cos(theta_delta),
-                - radius_delta * Math.sin(theta_delta)
-            ]);
-        }
+      // Interpolate steps. This is necessary to emulate how a Sisbot will plot the coordinates
+      let theta_delta  = thr_commands[i][0] - thr_commands[i-1][0];
+      let radius_delta = thr_commands[i][1] - thr_commands[i-1][1];
+      num_substeps = Math.abs((theta_delta * (180/Math.PI)/360) * substeps_per_revolution);
+      for (var j = 0; j < num_substeps; j++) {
+        path.push([
+          max_r * (thr_commands[i-1][1] + (j/num_substeps) * radius_delta) * Math.cos(thr_commands[i-1][0] + (j/num_substeps) * theta_delta),
+          max_r * (thr_commands[i-1][1] + (j/num_substeps) * radius_delta) * Math.sin(thr_commands[i-1][0] + (j/num_substeps) * theta_delta)
+        ]);
+      }
     }
+
+    // Convert starting point from Theta-Rho to XY
+    path.push([
+      max_r * thr_commands[num_thr_commands-1][1] * Math.cos(thr_commands[num_thr_commands-1][0]),
+      max_r * thr_commands[num_thr_commands-1][1] * Math.sin(thr_commands[num_thr_commands-1][0])
+    ]);
 
     return path;
   }

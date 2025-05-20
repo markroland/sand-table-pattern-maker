e9e9361acf04f6662653e32756aabf33cad6ff5e
Mark Roland <radiowhir@gmail.com>
2025-05-19 20:15:14 -0700

    Updated vite to be aware of environment to set base

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
-export default defineConfig({
-  base: '/sand-table-pattern-maker/',
-  root: 'src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        env: resolve(__dirname, 'src/js/env.js'),
+export default defineConfig(({ mode }) => {
+  const isLocalhost = mode === 'development';
+  return {
+    base: isLocalhost ? '/' : '/sand-table-pattern-maker/',
+    root: 'src',
+    publicDir: '../public',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'src/index.html'),
+          env: resolve(__dirname, 'src/js/env.js'),
+        },
       },
     },
-  },
-  server: {
-    open: true,
-  },
-  optimizeDeps: {
-    include: ['@markroland/path-helper'],
-  },
+    server: {
+      open: true,
+    },
+    optimizeDeps: {
+      include: ['@markroland/path-helper'],
+    },
+  };
 });
